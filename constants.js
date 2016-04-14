7ea3deae8999329639af8f5ac0bbe895ab117946
jdruid <joshdrew@gmail.com>
2016-04-14 15:26:20 -0400

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,7 +1,7 @@
 exports.adalConfiguration = {
   authority: 'https://login.microsoftonline.com/common',
-  clientID: '7dd0287a-e0ac-44f5-94de-878690e97ef6',
-  clientSecret: '79UazSXH/H7i4ImKtVydFuTXnUKCF4y+Idc7bp/BdfM=',
+  clientID: '',
+  clientSecret: '',
   redirectUri: 'http://localhost:3000/callback'
 };
 
